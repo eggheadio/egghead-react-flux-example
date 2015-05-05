8d91b7546a94de4d4be67168cf2b7ad2caa23147
ijones16 <ijones16@github.com>
2015-05-05 00:19:03 -0700

    broken change. saving state

--- src/js/components/header/app-cartsummary.js
+++ src/js/components/header/app-cartsummary.js
@@ -1,8 +1,19 @@
 /** @jsx React.DOM */
+var React = require('react');
+var Link = require('react-router-component').Link;
+
 var CartSummary =
   React.createClass({
     render:function(){
-      return ()
+      return (
+        <div>
+          <Link
+            href="/cart"
+            className="btn btn-success">
+            Cart Items: QTY / $COST
+            </Link>
+        </div>
+        )
     }
   });
   module.exports = CartSummary;
