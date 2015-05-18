ecae4910175613905f9e6938df0d7c7719ab0406
Kevin Kirsche <Kev.Kirsche+GitHub@gmail.com>
2015-05-18 16:31:47 -0400

    Update the store to use the newer dispatcher pattern from 02 dispatcher
    Update the store to use the newer dispatcher pattern from 02 dispatcher

--- src/js/stores/app-store.js
+++ src/js/stores/app-store.js
@@ -1,7 +1,6 @@
 var AppDispatcher = require('../dispatchers/app-dispatcher');
 var AppConstants = require('../constants/app-constants');
-var merge = require('react/lib/merge');
-var EventEmitter = require('events').EventEmitter;
+var AppStore = require('events').EventEmitter;
 
 var CHANGE_EVENT = "change";
 
@@ -46,55 +45,46 @@ function _addItem(item){
   }
 }
 
-var AppStore = merge(EventEmitter.prototype, {
-  emitChange:function(){
-    this.emit(CHANGE_EVENT)
-  },
+AppStore.emitChange = function () {
+  this.emit(CHANGE_EVENT);
+};
 
-  addChangeListener:function(callback){
-    this.on(CHANGE_EVENT, callback)
-  },
+AppStore.addChangeListener = function (callback) {
+  this.on(CHANGE_EVENT, callback);
+};
 
-  removeChangeListener:function(callback){
-    this.removeListener(CHANGE_EVENT, callback)
-  },
+AppStore.removeChangeListener = function (callback) {
+  this.removeListener(CHANGE_EVENT, callback);
+};
 
-  getCart:function(){
-    return _cartItems
-  },
+AppStore.getCart = function () {
+  return _cartItems;
+};
 
-  getCatalog:function(){
-    return _catalog
-  },
+AppStore.getCatalog = function () {
+  return _catalog;
+};
 
-  dispatcherIndex:AppDispatcher.register(function(payload){
-    var action = payload.action; // this is our action from handleViewAction
-    switch(action.actionType){
+AppStore.dispatcherIndex = function () {
+  AppDispatcher.register(function (payload) {
+    var action = payload.action; // this is our action from handleAction
+    switch (action.actionType) {
       case AppConstants.ADD_ITEM:
         _addItem(payload.action.item);
         break;
-
       case AppConstants.REMOVE_ITEM:
         _removeItem(payload.action.index);
         break;
-
       case AppConstants.INCREASE_ITEM:
         _increaseItem(payload.action.index);
         break;
-
       case AppConstants.DECREASE_ITEM:
         _decreaseItem(payload.action.index);
-        break;
     }
     AppStore.emitChange();
 
     return true;
-  })
-})
+  });
+};
 
 module.exports = AppStore;
-
-
-
-
-
